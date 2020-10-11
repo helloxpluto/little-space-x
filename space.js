c38936a48f8fbcce67af5649eb3441765fc1b48e
helloxpluto <deplanetart@gmail.com>
2020-10-11 08:57:58 -0700

    gonna have to figure out a different way

--- space.js
+++ space.js
@@ -10,7 +10,7 @@ const restart = document.querySelector('.restart')
 //word
 
 // make a spacewords array.
-let spacewords = [ 'Space', 'Earth', 'Solar System', 'Jupiter', 'Mars', 'quarter moon', 'Neptune', 'moon', 'gibbous moon', 'Mercury', 'Pluto', 'half moon', 'Saturn', 'Venus', 'crescent moon', 'Uranus', 'planet', 'axial tilt', 'asteroid belt', 'asteroid', 'black hole', 'big bang theory', 'astronaut', 'comet', 'binary star', 'astronomer', 'astronomy', 'elliptical orbit', 'density', 'constellation', 'deep space', 'cosmonaut', 'cosmos', 'dwarf planet', 'crater', 'dwarf star',  'equinox', 'inner planets', 'eclipse', 'ecliptic', 'inferior planets', 'galaxy', 'lunar', 'falling star', 'meteorite', 'meteor', 'meteor shower', 'meteoroid', 'lens', 'gravity', 'full moon', 'inertia', 'Milky Way', 'mass', 'magnitude', 'outer planets', 'nebula', 'orbit', 'shooting star', 'rocket', 'solar', 'space exploration', 'solstice', 'star', 'total eclipse', 'umbra', 'vernal equinox', 'sky', 'satellite', 'solar system', 'new moon', 'penumbra', 'solar wind', 'light year', 'rings', 'partial eclipse', 'observatory', 'phase', 'orbital inclination', 'universe', 'zodiac', 'space station', 'sun', 'starlight', 'telescope']
+let spacewords = [ 'space', 'earth', 'solar system', 'jupiter', 'mars', 'quarter moon', 'neptune', 'moon', 'gibbous moon', 'mercury', 'pluto', 'half moon', 'saturn', 'venus', 'crescent moon', 'uranus', 'planet', 'axial tilt', 'asteroid belt', 'asteroid', 'black hole', 'big bang theory', 'astronaut', 'comet', 'binary star', 'astronomer', 'astronomy', 'elliptical orbit', 'density', 'constellation', 'deep space', 'cosmonaut', 'cosmos', 'dwarf planet', 'crater', 'dwarf star',  'equinox', 'inner planets', 'eclipse', 'ecliptic', 'inferior planets', 'galaxy', 'lunar', 'falling star', 'meteorite', 'meteor', 'meteor shower', 'meteoroid', 'lens', 'gravity', 'full moon', 'inertia', 'milky way', 'mass', 'magnitude', 'outer planets', 'nebula', 'orbit', 'shooting star', 'rocket', 'solar', 'space exploration', 'solstice', 'star', 'total eclipse', 'umbra', 'vernal equinox', 'sky', 'satellite', 'solar system', 'new moon', 'penumbra', 'solar wind', 'light year', 'rings', 'partial eclipse', 'observatory', 'phase', 'orbital inclination', 'universe', 'zodiac', 'space station', 'sun', 'starlight', 'telescope']
 
 // set up a mathfloor math random on the array of spacewords call the output of that phraseRandom 
 let phraseRandom = spacewords[Math.floor(Math.random()* spacewords.length)]
@@ -19,21 +19,35 @@ let phraseRandom = spacewords[Math.floor(Math.random()* spacewords.length)]
 // push phraseRandom into phrase as an array of each letter
 // turn number of letters into blank spaces
 //give those blank spaces an underline or different color background or other style change.
-let phraseSpaced = []
-for (let i = 0; i < phraseRandom.length; i++) {
-    phraseSpaced[i] = (' _ ')
-    
-}
-// console.log(phraseSpaced);
-phrase = phraseRandom.split()
-console.log(phrase);
-// let phraseBlank = []
 // let phraseSpaced = []
 // for (let i = 0; i < phraseRandom.length; i++) {
-//     phraseSpaced[i] = phraseRandom[i];
-//     phraseBlank[i] = (' _ ')
-   
- 
-//     console.log(phraseSpaced);
+//     phraseSpaced[i] = (' _ ')
+    
 // }
-// phrase.innerText = phraseSpaced
+// console.log(phraseSpaced);
+// phrase = phraseRandom.length
+// console.log(phrase);
+let phraseBlank = []
+for (let i = 0; i < phraseRandom.length; i++) {
+    phraseBlank[i] = (' _ ') 
+    // console.log(phraseBlank);
+}
+
+phrase.innerText = phraseBlank
+
+alphabet.addEventListener('click', (event) => {
+    event.preventDefault()
+    // console.log(event)
+
+    for (let i = 0; i < phraseRandom.length; i++) {
+        if (event.target.innerHTML !== phraseRandom[i]) {
+            console.log("yess");
+        } else {
+            console.log('Noooo');
+        }
+       
+       
+    
+    }
+// console.log(event.target.innerHTML);
+})

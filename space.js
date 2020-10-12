83612668566d5a6c68bf4a07342cd34c74938ef5
helloxpluto <deplanetart@gmail.com>
2020-10-12 05:21:14 -0700

    having a little trouble

--- space.js
+++ space.js
@@ -1,53 +1,159 @@
 // assing all my variables and gets.
-const fuel = document.querySelectorAll('.fuel')
+const fuel = document.querySelectorAll('.fuel');
 
-let phrase = document.querySelector('#phrase')
+let phrase = document.querySelector('.phrase');
 
-const alphabet = document.querySelector('.alphabet')
+const alphabet = document.querySelector('.alphabet');
 
-const restart = document.querySelector('.restart')
+const restart = document.querySelector('.restart');
 
+const winTxt = document.querySelector('.winnertxt');
 //word
 
-// make a spacewords array.
-let spacewords = [ 'space', 'earth', 'solar system', 'jupiter', 'mars', 'quarter moon', 'neptune', 'moon', 'gibbous moon', 'mercury', 'pluto', 'half moon', 'saturn', 'venus', 'crescent moon', 'uranus', 'planet', 'axial tilt', 'asteroid belt', 'asteroid', 'black hole', 'big bang theory', 'astronaut', 'comet', 'binary star', 'astronomer', 'astronomy', 'elliptical orbit', 'density', 'constellation', 'deep space', 'cosmonaut', 'cosmos', 'dwarf planet', 'crater', 'dwarf star',  'equinox', 'inner planets', 'eclipse', 'ecliptic', 'inferior planets', 'galaxy', 'lunar', 'falling star', 'meteorite', 'meteor', 'meteor shower', 'meteoroid', 'lens', 'gravity', 'full moon', 'inertia', 'milky way', 'mass', 'magnitude', 'outer planets', 'nebula', 'orbit', 'shooting star', 'rocket', 'solar', 'space exploration', 'solstice', 'star', 'total eclipse', 'umbra', 'vernal equinox', 'sky', 'satellite', 'solar system', 'new moon', 'penumbra', 'solar wind', 'light year', 'rings', 'partial eclipse', 'observatory', 'phase', 'orbital inclination', 'universe', 'zodiac', 'space station', 'sun', 'starlight', 'telescope']
+// make a words array.
+let words = [
+	'space',
+	'earth',
+	'solar system',
+	'jupiter',
+	'mars',
+	'quartermoon',
+	'neptune',
+	'moon',
+	'moon',
+	'mercury',
+	'pluto',
+	'halfmoon',
+	'saturn',
+	'venus',
+	'crescentmoon',
+	'uranus',
+	'planet',
+	'axialtilt',
+	'asteroidbelt',
+	'asteroid',
+	'blackhole',
+	'bigbangtheory',
+	'astronaut',
+	'comet',
+	'binarystar',
+	'astronomer',
+	'astronomy',
+	'elliptical',
+	'density',
+	'constellation',
+	'deepspace',
+	'cosmonaut',
+	'cosmos',
+	'dwarfplanet',
+	'crater',
+	'equinox',
+	'innerplanets',
+	'eclipse',
+	'ecliptic',
+	'uuuplanets',
+	'galaxy',
+	'lunar',
+	'fallingstar',
+	'meteorite',
+	'meteor',
+	'meteorshower',
+	'meteoroid',
+	'lens',
+	'gravity',
+	'fullmoon',
+	'inertia',
+	'milkyway',
+	'mass',
+	'magnitude',
+	'outerplanets',
+	'nebula',
+	'orbit',
+	'shootingstar',
+	'rocket',
+	'solar',
+	'exploration',
+	'solstice',
+	'star',
+	'eclipse',
+	'umbra',
+	'equinox',
+	'sky',
+	'satellite',
+	'solar system',
+	'new moon',
+	'penumbra',
+	'solar wind',
+	'light',
+	'rings',
+	'observatory',
+	'phase',
+	'orbital',
+	'universe',
+	'zodiac',
+	'spacestation',
+	'sun',
+	'starlight',
+	'telescope',
+];
 
-// set up a mathfloor math random on the array of spacewords call the output of that phraseRandom 
-let phraseRandom = spacewords[Math.floor(Math.random()* spacewords.length)]
+// set up a mathfloor math random on the array of spacewords call the output of that phraseRandom
+let phraseRandom = words[Math.floor(Math.random() * words.length)];
 // console.log(phraseRandom);
 
 // push phraseRandom into phrase as an array of each letter
 // turn number of letters into blank spaces
 //give those blank spaces an underline or different color background or other style change.
-// let phraseSpaced = []
+// let phraseSpaced = [];
 // for (let i = 0; i < phraseRandom.length; i++) {
-//     phraseSpaced[i] = (' _ ')
-    
+// 	phraseSpaced[i] = ' _ ';
 // }
-// console.log(phraseSpaced);
-// phrase = phraseRandom.length
-// console.log(phrase);
-let phraseBlank = []
-for (let i = 0; i < phraseRandom.length; i++) {
-    phraseBlank[i] = (' _ ') 
-    // console.log(phraseBlank);
+const rightLetters = [' '];
+const wrongLetters = [' '];
+function displayPhrase() {
+	phrase.innerHTML = `${phraseRandom
+		.split('')
+		.map(
+			(letter) => `
+                <span class = "letter">
+                  ${rightLetters.includes(letter) ? letter : ''}
+                </span>
+            `
+		)
+		.join('')}
+    `;
+	const innerPhrase = phrase.innerText.replace(/\n/g, '');
+	if (innerPhrase === phraseRandom) {
+		finalMessage.innerText = 'Congradulations!';
+	}
 }
 
-phrase.innerText = phraseBlank
+function showNotification() {
+	Notification.classList.add('show');
+}
 
 alphabet.addEventListener('click', (event) => {
-    event.preventDefault()
-    // console.log(event)
+	event.preventDefault();
+	const letter = event.target.value;
+	if (phraseRandom.includes(letter)) {
+		if (!rightLetters.includes(letter)) {
+			rightLetters.push(letter);
 
-    for (let i = 0; i < phraseRandom.length; i++) {
-        if (event.target.innerHTML !== phraseRandom[i]) {
-            console.log("yess");
-        } else {
-            console.log('Noooo');
-        }
-       
-       
-    
-    }
-// console.log(event.target.innerHTML);
-})
+			displayPhrase();
+		} else {
+			showNotification();
+		}
+	} else {
+		if (!wrongLetters.includes(letter)) {
+			wrongLetters.push(letter);
+
+			updateWrongLetter();
+		} else {
+			showNotification();
+		}
+	}
+	
+});
+
+displayPhrase();
+console.log(rightLetters);

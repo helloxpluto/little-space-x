9bf50cfc841ac0a3810d55809624c21ce286c6bb
helloxpluto <deplanetart@gmail.com>
2020-10-13 23:54:22 -0700

    final commit

--- space.js
+++ space.js
@@ -1,92 +1,88 @@
 // naming all my variables and gets.
 const fuel = document.querySelectorAll('.fuel');
 
+const cell = document.querySelectorAll('.cell');
+
 let phrase = document.querySelector('.phrase');
 
 const alphabet = document.querySelector('.alphabet');
 
 const restart = document.querySelector('.restart');
 
-const winTxt = document.querySelector('.winnertxt');
-
-const notification = document.querySelector('.notify');
-
 const under = document.querySelector('.under');
 
 let popup = document.querySelector('.popup-container');
-
-// const notification = document.getElementById('notification-container');
 
 const finalMessage = document.querySelector('#final-message');
 
 // make a words array.
 let words = [
-	'space']
-// 	'earth',
-// 	'jupiter',
-// 	'mars',
-// 	'neptune',
-// 	'moon',
-// 	'moon',
-// 	'mercury',
-// 	'pluto',
-// 	'saturn',
-// 	'venus',
-// 	'uranus',
-// 	'planet',
-// 	'asteroid',
-// 	'blackhole',
-// 	'astronaut',
-// 	'comet',
-// 	'binarystar',
-// 	'astronomer',
-// 	'astronomy',
-// 	'elliptical',
-// 	'density',
-// 	'constellation',
-// 	'deepspace',
-// 	'cosmonaut',
-// 	'cosmos',
-// 	'crater',
-// 	'equinox',
-// 	'eclipse',
-// 	'ecliptic',
-// 	'planets',
-// 	'galaxy',
-// 	'lunar',
-// 	'meteorite',
-// 	'meteor',
-// 	'meteoroid',
-// 	'lens',
-// 	'gravity',
-// 	'fullmoon',
-// 	'inertia',
-// 	'milkyway',
-// 	'mass',
-// 	'magnitude',
-// 	'nebula',
-// 	'orbit',
-// 	'rocket',
-// 	'solar',
-// 	'exploration',
-// 	'solstice',
-// 	'star',
-// 	'eclipse',
-// 	'umbra',
-// 	'equinox',
-// 	'sky',
-// 	'satellite',
-// 	'penumbra',
-// 	'light',
-// 	'rings',
-// 	'observatory',
-// 	'orbital',
-// 	'universe',
-// 	'zodiac',
-// 	'sun',
-// 	'starlight',
-// 	'telescope',
-// ];
+	'space',
+	'earth',
+	'jupiter',
+	'mars',
+	'neptune',
+	'moon',
+	'moon',
+	'mercury',
+	'pluto',
+	'saturn',
+	'venus',
+	'uranus',
+	'planet',
+	'asteroid',
+	'blackhole',
+	'astronaut',
+	'comet',
+	'binarystar',
+	'astronomer',
+	'astronomy',
+	'elliptical',
+	'density',
+	'constellation',
+	'deepspace',
+	'cosmonaut',
+	'cosmos',
+	'crater',
+	'equinox',
+	'eclipse',
+	'ecliptic',
+	'planets',
+	'galaxy',
+	'lunar',
+	'meteorite',
+	'meteor',
+	'meteoroid',
+	'lens',
+	'gravity',
+	'fullmoon',
+	'inertia',
+	'milkyway',
+	'mass',
+	'magnitude',
+	'nebula',
+	'orbit',
+	'rocket',
+	'solar',
+	'exploration',
+	'solstice',
+	'star',
+	'eclipse',
+	'umbra',
+	'equinox',
+	'sky',
+	'satellite',
+	'penumbra',
+	'light',
+	'rings',
+	'observatory',
+	'orbital',
+	'universe',
+	'zodiac',
+	'sun',
+	'starlight',
+	'telescope',
+];
 
 // set up a mathfloor math random on the array of spacewords call the output of that phraseRandom
 let phraseRandom = words[Math.floor(Math.random() * words.length)];
@@ -107,11 +103,12 @@ const rightLetters = [];
 
 const wrongLetters = [];
 
+//place phrase letters into phrase
 function displayPhrase() {
 	phrase.innerHTML = `${phraseRandom
 		.split('')
 		.map(
-			letter => `
+			(letter) => `
                 <span class = "letter">
                   ${rightLetters.includes(letter) ? letter : ''}
                 </span>
@@ -119,36 +116,28 @@ function displayPhrase() {
 		)
 		.join('')}
     `;
-	
-	const innerPhrase = phrase.innerText.replace(/\n/g, '');
-	
-	if (innerPhrase === phraseRandom) {
-		finalMessage.innerText = 'YOU WON !!';
-		popup.style.display = 'flex';
-	}
 }
 
-
-
-  function updateWrongLetter() {
+function updateWrongLetters() {
 	wrongLetters.innerhtml = `
 	${wrongLetters.length > 0 ? '<p>wrong</p>' : ''}
-	${wrongLetters.map((letter) => `span${letter}</span>`)}
+	${wrongLetters.map((letter) => `<span>${letter}</span>`)}
 	`;
 
-	cel.forEach((cell, index) => {
+	cell.forEach((part, index) => {
 		const errors = wrongLetters.length;
 
 		if (index < errors) {
-			cell.style.display = 'block';
+			part.style.display = 'none';
 		} else {
-			cell.style.display = 'none';
+			part.style.display = 'block';
 		}
 	});
-  }
 
-function showNotification() {
-	notification.classList.add('show');
+	if (wrongLetters.length === cell.length) {
+		finalMessage.innerText = 'you lost... TRY AGAIN. ';
+		popup.style.display = 'flex';
+	}
 }
 
 alphabet.addEventListener('click', (event) => {
@@ -159,17 +148,17 @@ alphabet.addEventListener('click', (event) => {
 			rightLetters.push(letter);
 
 			displayPhrase();
-		} else {
-			showNotification();
 		}
 	} else {
 		if (!wrongLetters.includes(letter)) {
 			wrongLetters.push(letter);
 
-			updateWrongLetter();
-		} else {
-			showNotification();
+			updateWrongLetters();
 		}
+	}
+	if (phraseRandom.length === rightLetters.length) {
+		finalMessage.innerText = 'YOU WON !!';
+		popup.style.display = 'flex';
 	}
 });
 
